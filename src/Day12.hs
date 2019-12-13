da9de9e55a268083b81fd72342b140838c6239bf
Patrick Teubner <patrick.teubner@rub.de>
2019-12-13 01:30:19 +0100

    Rename invert to transpose

--- src/Day12.hs
+++ src/Day12.hs
@@ -9,7 +9,7 @@ part1 :: Int
 part1 = sum $ fmap energy $ iterate stepTime moons !! 1000
 
 part2 :: Int
-part2 = foldr lcm 1 $ period <$> traverse invert moons
+part2 = foldr lcm 1 $ period <$> traverse transpose moons
 
 period :: (Eq a, Num a) => [(a, a)] -> Int
 period as = go 1 (stepTime as) where go i xs = if xs == as then i else go (i + 1) $ stepTime xs
@@ -32,8 +32,8 @@ stepVelocity = foldr (\(other, _) (x, v) -> (x, v + accelerate other x))
 accelerate :: Num a => a -> a -> a
 accelerate v w = signum (v - w)
 
-invert :: Body -> Vector (Int, Int)
-invert (Vector x y z, Vector u v w) = Vector (x, u) (y, v) (z, w)
+transpose :: Body -> Vector (Int, Int)
+transpose (Vector x y z, Vector u v w) = Vector (x, u) (y, v) (z, w)
 
 moons :: [Body]
 moons = initBody <$> [(-16, 15, -9), (-14, 5, 4), (2, 0, 6), (-3, 18, 9)]
