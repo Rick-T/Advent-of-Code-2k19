ab60060cd55caf38527400c469b1b4f60c4b12eb
Patrick Teubner <patrick.teubner@rub.de>
2019-12-23 13:58:54 +0100

    Day 23

--- app/Main.hs
+++ app/Main.hs
@@ -22,6 +22,7 @@ import Day19
 import Day20
 import Day21
 import Day22
+import Day23
 
 import Control.Monad (forM_)
 import System.Environment (getArgs)
@@ -61,6 +62,7 @@ getFuncs 19 = printable (Day19.part1, Day19.part2)
 getFuncs 20 = printable (Day20.part1, Day20.part2)
 getFuncs 21 = printable (Day21.part1, Day21.part2)
 getFuncs 22 = printable (Day22.part1, Day22.part2)
+getFuncs 23 = printable (Day23.part1, Day23.part2)
 
 printable :: (Show a, Show b) => (IO a, IO b) -> (IO (), IO ())
 printable (a, b) = (print =<< a, print =<< b)
---
+++ src/Day23.hs
@@ -0,0 +1,114 @@
+module Day23 where
+
+import Paths_Advent_of_Code_2k19
+import Common.Intcode
+import Control.Monad.State.Lazy
+import Control.Monad.RWS.Lazy
+import Data.IntMap.Strict as M
+import Data.Sequence as S
+
+data Network = Net { network :: IntMap Computer, packetQueue :: Seq Packet, nat :: Maybe Packet} deriving Show
+
+type NetworkState = State Network
+
+type Packet = (Int, Int, Int)
+
+part1 :: IO Packet
+part1 = head . natOutput <$> loadComputer "input/Day23.txt"
+
+part2 :: IO (Maybe Packet)
+part2 = firstDup . natOutput <$> loadComputer "input/Day23.txt"
+
+firstDup :: Eq a => [a] -> Maybe a
+firstDup []  = Nothing
+firstDup [a] = Nothing
+firstDup (a : b : cs)
+  | a == b    = Just a
+  | otherwise = firstDup $ b : cs
+
+natOutput :: Computer -> [Packet]
+natOutput = evalState stepNetwork . initNetwork
+
+stepNetwork :: NetworkState [Packet]
+stepNetwork = do
+  mPckt <- consumePacket
+  case mPckt of
+    Nothing -> do
+      pckt <- searchPacket
+      case pckt of
+        [] -> sendNat
+        ps -> do
+          addPackets ps
+          stepNetwork
+    Just pckt -> do
+      handlePacket pckt
+      stepNetwork
+
+sendNat :: NetworkState [Packet]
+sendNat = do
+  toSend <- gets nat
+  case toSend of
+    Nothing        -> stepNetwork
+    Just (_, x, y) -> do
+      addPacket (0, x, y)
+      ((0, x, y) :) <$> stepNetwork
+
+searchPacket :: NetworkState [Packet]
+searchPacket = searchPacket' 0
+
+searchPacket' :: Int -> NetworkState [Packet]
+searchPacket' ptr = do
+  pckts <- receivePackets ptr $ runUntil isInput
+  case pckts of
+    [] -> if ptr == 49 then return [] else searchPacket' (ptr + 1)
+    _  -> return pckts
+
+handlePacket :: Packet -> NetworkState ()
+handlePacket (255, x, y) = modify $ \n -> n { nat = Just (0, x, y) }
+handlePacket (d  , x, y) = addPackets =<< receivePackets d (feedInput [x, y])
+
+receivePackets :: Int -> ComputerState a -> NetworkState [Packet]
+receivePackets d comm = do
+  output <- snd <$> runComputer d comm
+  case output of
+    []           -> return []
+    partialPckts -> do
+      let missingData = (3 - (Prelude.length partialPckts `mod` 3)) `mod` 3
+      (_, trailing) <- runComputer d (nextOutputs missingData)
+      return $ toPackets $ partialPckts ++ trailing
+
+consumePacket :: NetworkState (Maybe Packet)
+consumePacket = do
+  q <- gets packetQueue
+  case q of
+    Empty         -> return Nothing
+    (pckt :<| q') -> do
+      modify $ \n -> n { packetQueue = q' }
+      return $ Just pckt
+
+addPackets :: [Packet] -> NetworkState ()
+addPackets = mapM_ addPacket
+
+addPacket :: Packet -> NetworkState ()
+addPacket packet = do
+  q <- gets packetQueue
+  modify $ \n -> n { packetQueue = q |> packet }
+
+runComputer :: Int -> ComputerState a -> NetworkState (a, [Val])
+runComputer d com = do
+  net <- gets network
+  let c             = net ! d
+  let (a, c', outs) = runRWS com (-1) c
+  modify $ \n -> n { network = M.insert d c' net }
+  return (a, outs)
+
+initNetwork :: Computer -> Network
+initNetwork c =
+  let network = M.fromList $ fmap (\(d, c) -> (d, fst $ execRWS (feedInput [d]) (-1) c)) $ Prelude.zip [0 .. 49] $ repeat c in Net network mempty Nothing
+
+toPackets :: [Val] -> [Packet]
+toPackets []                 = []
+toPackets (d : x : y : rest) = (d, x, y) : toPackets rest
+
+destination :: Packet -> Int
+destination (d, _, _) = d
